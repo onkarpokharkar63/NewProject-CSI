f8eb47da78579da4b8f811eae04ee4932f2fb8fc
Onkar Pokharkar <onkarpokharkar63@gmail.com>
2022-03-23 15:11:07 +0530

    initial commit

---
+++ GithubNewProject/GithubNewProject/src/main/java/com/csi/controllerfile/ControllerApplication.java
@@ -0,0 +1,15 @@
+package com.csi.controllerfile;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1")
+public class ControllerApplication {
+
+    @GetMapping
+    public String sayHello(){
+        return "Hello Akshay";
+    }
+}
